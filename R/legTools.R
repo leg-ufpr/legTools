d0af18e5de2f9e6879396de8ee13cba9e6e399c1
Fernando Mayer <fernandomayer@gmail.com>
2015-08-12 17:36:53 -0300

    general package documentation

---
+++ R/legTools.R
@@ -0,0 +1,10 @@
+##' @title Convenience Functions, Small GUI to Teach Statistics and Some
+##'     Datasets.
+##'
+##' @description legTools is a collection of R functions and datasets,
+##'     maintained by the LEG group (Statistics and Geoinformation
+##'     Laboratory, Laboratório de Estatística e Geoinformação).
+##'
+##' @docType package
+##' @name legTools
+NULL
