906036c51fa58944e48ad4ad93356f10100f1ef5
Walmes Zeviani <walmeszeviani@gmail.com>
2015-08-19 11:15:45 -0300

    Documentation of dataset Pimentel page 91 generated.

--- R/legTools.R
+++ R/legTools.R
@@ -32,7 +32,7 @@ NULL
 #' @format a \code{data.frame} with 20 records and 2 variables.
 #'
 #' @source Frederico, P. (2009). Curso de Estatística Experimental
-#' (15th ed.). Piracicaba, São Paulo: FEALQ.
+#' (15th ed.). Piracicaba, São Paulo: FEALQ. (page 62)
 #'
 #' @examples
 #'
@@ -79,5 +79,44 @@ NULL
 #'      groups=block, type="o",
 #'      ylab=expression(Yield~(t~ha^{-1})),
 #'      xlab="Variety")
+#'
+NULL
+
+#' @name plowing
+#'
+#' @title Plowing level on corn yield
+#'
+#' @description These data are from an experiment done by the engineer
+#' Duvilio Ometto to study the effect of plowing level on corn yield. It
+#' was used 2 levels of plowing: normal (or superficial) and deep. The
+#' experiment was done in a randomized complete block design with 6
+#' blocks. Corn yield (t ha^{-1}) was recorded in each experimental unit
+#' but in this experiment there was 2 experimental units for each factor
+#' level in each block.
+#'
+#' \itemize{
+#'     \item block a categorical unordered factor with 6 levels.
+#'     \item plow a categorical unordered factor with 2 levels.
+#'     \item yield corn yield (kg in 200 m^{2} of area).
+#' }
+#'
+#' @docType data
+#'
+#' @keywords datasets
+#'
+#' @usage data(plowing)
+#'
+#' @format a \code{data.frame} with 24 records and 3 variables.
+#'
+#' @source Frederico, P. (2009). Curso de Estatística Experimental
+#' (15th ed.). Piracicaba, São Paulo: FEALQ. (page 91)
+#'
+#' @examples
+#' require(lattice)
+#' data(potatoyield)
+#'
+#' xyplot(yield~plow|block, data=plowing, type=c("p", "a"),
+#'        ylab=expression(Yield~(t~ha^{-1})),
+#'        xlab="Plowing level")
 #'
 NULL
