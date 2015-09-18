f703d2e68ee13a7f7566c75cbc873a958c87d434
Walmes Zeviani <walmeszeviani@gmail.com>
2015-09-18 12:09:32 -0300

    Add wgpigs.txt to upload to public folder.

--- data-raw/wgpigs.R
+++ data-raw/wgpigs.R
@@ -21,3 +21,18 @@ rm(list=ls())
 load("../data/wgpigs.RData")
 ls()
 str(wgpigs)
+
+##----------------------------------------------------------------------
+
+write.table(x=wgpigs, file="wgpigs.txt",
+            sep="\t", quote=FALSE, row.names=FALSE)
+
+## Port and IP.
+u <- scan(n=2, what=character())
+cmd <- paste0("scp -P ", u[1], " wgpigs.txt leg@", u[2],
+              ":/home/leg/public_html/legTools/dataset")
+system(cmd)
+
+url <- "http://blog.leg.ufpr.br/~leg/legTools/dataset/wgpigs.txt"
+browseURL(url)
+wgpigs <- read.table(file=url, header=TRUE, sep="\t")
