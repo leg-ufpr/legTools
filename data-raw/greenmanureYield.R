c48da3859ec60351810d419772e2ce166048d010
bruna wundervald <brunadaviesw@gmail.com>
2015-10-15 19:48:56 -0300

    greenmanureYield.R improvement

--- data-raw/greenmanureYield.R
+++ data-raw/greenmanureYield.R
@@ -2,10 +2,36 @@
 ## Data generation.
 
 x <- read.table(file = 'clipboard', sep = "\t", header=TRUE)
-greenmanureYield <- dput(x)
+dput(x)
+greenmanureYield <- structure(list(ano = c(1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 2L, 2L, 
+2L, 2L, 2L, 2L, 2L, 2L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 2L, 2L, 
+2L, 2L, 2L, 2L, 2L, 2L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 2L, 2L, 
+2L, 2L, 2L, 2L, 2L, 2L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 2L, 2L, 
+2L, 2L, 2L, 2L, 2L, 2L), bloco = structure(c(1L, 1L, 1L, 1L, 
+1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 2L, 2L, 2L, 2L, 
+2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 3L, 3L, 3L, 3L, 
+3L, 3L, 3L, 3L, 3L, 3L, 3L, 3L, 3L, 3L, 3L, 3L, 4L, 4L, 4L, 4L, 
+4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L, 4L), .Label = c("i", 
+"ii", "iii", "iv"), class = "factor"), cultura = structure(c(6L, 
+3L, 2L, 4L, 8L, 7L, 1L, 5L, 6L, 3L, 2L, 4L, 8L, 7L, 1L, 5L, 6L, 
+3L, 2L, 4L, 8L, 7L, 1L, 5L, 6L, 3L, 2L, 4L, 8L, 7L, 1L, 5L, 6L, 
+3L, 2L, 4L, 8L, 7L, 1L, 5L, 6L, 3L, 2L, 4L, 8L, 7L, 1L, 5L, 6L, 
+3L, 2L, 4L, 8L, 7L, 1L, 5L, 6L, 3L, 2L, 4L, 8L, 7L, 1L, 5L), 
+.Label = c("Crotalaria grantiana", 
+"Crotalaria juncea", "Feijao-de-porco", "Guandu", "Milho", "Mucuna preta", 
+"Soja", "Tephrosia Candida"), class = "factor"), materiaverde = c(86.8, 
+44, 102.4, 68.4, 34, 33, 25.8, 138.8, 90.2, 83.8, 120.2, 91, 
+57.2, 33.6, 77, 110.2, 76.8, 56.6, 90.8, 55.2, 32.4, 34.8, 21.6, 
+106.4, 94, 72.2, 104.6, 78.8, 54, 33.2, 62.4, 80, 88.6, 52.4, 
+92, 49, 24.4, 32, 19.2, 108, 86.4, 88.6, 112, 83.4, 50.8, 33.4, 
+63.6, 92, 81.6, 52.2, 84.8, 61.2, 30, 33.6, 21, 81.8, 82.2, 83.2, 
+113.6, 91.2, 46.2, 42.6, 63.4, 90.6)), .Names = c("ano", "bloco", 
+"cultura", "materiaverde"), class = "data.frame", row.names = c(NA, 
+-64L))
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
 names(greenmanureYield) <- c("year", "block", "culture", "greenmanure")
 greenmanureYield$culture <- as.factor(c("mucuna pruriens", "jack bean", 
-      "crotalaria juncea", "pigeon pea", "tephrosia candida",
+        "crotalaria juncea", "pigeon pea", "tephrosia candida",
       "soy bean", "crotalaria grantiana", "corn"))
 str(greenmanureYield)
 save(greenmanureYield, file="../data/greenmanureYield.RData")
